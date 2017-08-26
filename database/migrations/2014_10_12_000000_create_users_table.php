dcdbccd3b8fe033294122817ed0c09684146c4e2
zoheir <zoheirkhonyagar@gmail.com>
2017-08-26 20:35:48 +0430

    resolve unique issue & user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('national_number' , 10)->unique();
+            $table->text('image')->nullable();
+            $table->string('type')->default('admin');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
