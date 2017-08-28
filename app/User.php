d889fd731a4b121e8c87cb8ac5015d4b3d2f5a17
zoheir <zoheirkhonyagar@gmail.com>
2017-08-28 16:03:12 +0430

    add fillable values for User Model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name' , 'last_name' , 'national_number' , 'image' , 'type' , 'password'
     ];
 
     /**
